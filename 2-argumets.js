8db0d9937ab2f463a03974d454ea7f6c6937f2e1
Alsowilson <wilsonokoth45@gmail.com>
2025-07-20 15:39:29 +0300

    Complete JavaScript fundamentals tasks

---
+++ 2-argumets.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+    console.log('No arguments');
+} else if {args.length === 1
+    console.log('Argument foundd');
+} else {
+    console.log('Arguments found');
+    
---
+++ 5-to_integer.js
@@ -0,0 +1,6 @@
+const num = Number.parseInt(process.argv{2});
+if (Number.isNaN(num)) {
+    console.log('Not a number');
+} else {
+    console.log(`My number: ${num}`);
+}
